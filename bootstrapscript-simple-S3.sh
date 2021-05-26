3b238619b809644e5ff0251858eedff73a9b3aa2
Ahmed HOSNI <ahmedhosni.contact@gmail.com>
2021-05-26 12:47:33 +0100

    add files

--- bootstrapscript-simple-S3.sh
+++ bootstrapscript-simple-S3.sh
@@ -4,6 +4,6 @@ yum install httpd -y
 service httpd start
 chkconfig httpd on
 cd /var/www/html
-echo "<html><h1>Hello ITPEAC Welcome To My Webpage</h1></html>" > index.html
-aws s3 mb s3://itpeac-awscsa-testbucketbootstrap-080920200955-liajfipuzefae
-aws s3 cp index.html s3://itpeac-awscsa-testbucketbootstrap-080920200955-liajfipuzefae
+echo "<html><h1>Hello Ghazela Technology Academy Welcome To My Webpage</h1></html>" > index.html
+aws s3 mb s3://ghazelatechnologyacademy-awscsa-testbucketbootstrap-080920200955-liajfipuzefae
+aws s3 cp index.html s3://ghazelatechnologyacademy-awscsa-testbucketbootstrap-080920200955-liajfipuzefae
